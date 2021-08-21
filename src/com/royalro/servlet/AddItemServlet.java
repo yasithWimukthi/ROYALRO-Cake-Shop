0c1b7ba5860c48c1e8090c40e058cdba7747bf27
ShalithAlahakoon <shalitha97068@gmail.com>
2021-08-22 01:29:58 +0530

    Insert Item function completed

--- src/com/royalro/servlet/AddItemServlet.java
+++ src/com/royalro/servlet/AddItemServlet.java
@@ -1,5 +1,6 @@
 package com.royalro.servlet;
 
+import com.royalro.service.ProductService;
 import com.royalro.service.UserAuthentication;
 
 import javax.servlet.RequestDispatcher;
@@ -14,6 +15,26 @@ import java.io.IOException;
 public class AddItemServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        String name = request.getParameter("ItemNameInput");
+        String price = request.getParameter("ItemPriceInput");
+        String category = request.getParameter("ItemCategoryInput");
+        String qty = request.getParameter("Qty");
+        String brand = request.getParameter("Brand");
+        String companyCode = request.getParameter("CompanyCode");
+        String description = request.getParameter("description");
+        String image = request.getParameter("ItemImage");
+// testings
+//        System.out.println(name);
+//        System.out.println(price);
+//        System.out.println(category);
+//        System.out.println(qty);
+//        System.out.println(brand);
+//        System.out.println(companyCode);
+//        System.out.println(description);
+//        System.out.println(image);
+
+        ProductService ps = new ProductService();
+        ps.addProduct(name,category,description,brand,companyCode,image,Float.parseFloat(price),Integer.parseInt(qty));
 
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/ItemManagementAdmin.jsp");
         dispatcher.forward(request, response);
